c4aa9c954a0f9c61e4eaf89f464325ff02eddda9
Caroline Lee <carolinechloelee@gmail.com>
2020-08-17 14:48:36 -0700

    Updates

---
+++ precache-manifest.d8d3cc940e609e7a1f5691ed492196d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142e09191f97b8496ede5c131c09445a",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "828cf7471fafd04aa787",
+    "url": "/React-Portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "b9945eb5897caf60ef8f",
+    "url": "/React-Portfolio/static/css/main.4b9910ca.chunk.css"
+  },
+  {
+    "revision": "828cf7471fafd04aa787",
+    "url": "/React-Portfolio/static/js/2.eb68ab8d.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/React-Portfolio/static/js/2.eb68ab8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9945eb5897caf60ef8f",
+    "url": "/React-Portfolio/static/js/main.6e54be1c.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  }
+]);
